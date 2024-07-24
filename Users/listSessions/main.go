5134233fd3e8d2ee478aadd1e14dd334d6acc298
choir27 <richardchoi54@gmail.com>
2024-07-24 15:46:28 -0400

    createAccount, createEmailPasswordSession, deleteSessions, createSMS, createUser, listSessions

---
+++ Users/listSessions/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/appwrite/sdk-for-go/client"
+	"github.com/appwrite/sdk-for-go/users"
+)
+
+func main() {
+	appwriteClient := client.NewClient()
+	appwriteClient.SetEndpoint("https://v16.appwrite.org/v1")
+	appwriteClient.SetProject("669c026b0002eaad620f")
+	appwriteClient.SetKey("standard_f8bdb93356c87ee8ed5284431e896cbb1c9bf40a57e6945c44a9b194901415506783d649c332a2d71daab0f3f9c2a790d3a7b4a94a882e9be81ace80a7114a0d3975fdf316f5c97059c7e7d82cfdf69a98ffaa163f8bfacf1aa484534614998e4077fcbd18f6357b9669f7385a740730ab7d71c68e0834b8cfbbe38c03073def")
+
+	service := users.NewUsers(appwriteClient)
+
+	response, error := service.Create(
+		"unique()",
+		users.WithCreateEmail("test@example.com"),
+		users.WithCreatePhone("+12065550100"),
+		users.WithCreatePassword("test1234"),
+		users.WithCreateName("test"),
+	)
+
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+
+	fmt.Println(response)
+}
