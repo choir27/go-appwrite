6c796d0c1012747ef4d848b53ced1c49bdf38415
choir27 <richardchoi54@gmail.com>
2024-07-25 15:52:42 -0400

    createAuth, createPhoneToken, createPhoneVerifiation, createRecovery, createSession, createVerification, deleteIdentity, deleteMFAauth, deleteSession, getMFAcodes, getSession, listLogs, listIdentities using GO appwrite SDK

---
+++ Account/getSession/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/appwrite/sdk-for-go/account"
+	"github.com/appwrite/sdk-for-go/client"
+)
+
+func main() {
+	appwriteClient := client.NewClient()
+	appwriteClient.SetEndpoint("https://v16.appwrite.org/v1")
+	appwriteClient.SetProject("669c026b0002eaad620f")
+
+	service := account.NewAccount(appwriteClient)
+
+	res, err := service.CreateEmailPasswordSession(
+		"email@example.com",
+		"example1234",
+	)
+
+	fmt.Println(res)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	response, error := service.GetSession(
+		"SESSION_ID",
+	)
+
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+
+	fmt.Println(response)
+}
